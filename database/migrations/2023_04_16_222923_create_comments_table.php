84e017b6162472eff8629299fe583354617a8782
Cian <cianashby1337@gmail.com>
2023-04-24 14:51:39 +0100

    Deleted M:N user schemas, user:comment/post is 1:M

--- database/migrations/2023_04_16_222923_create_comments_table.php
+++ database/migrations/2023_04_16_222923_create_comments_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->text('message');
             $table->timestamp('posted_on')->useCurrent();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
