fcb276c2efdaad6f6dc2d76173d4e15821f2df1a
Cian <cianashby1337@gmail.com>
2023-05-12 13:43:47 +0100

    New background w/transparent div for text contrast

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="background-image grid grid-cols-1 m-auto">
         <div class="flex text-gray-100 pt-10">
-            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+            <div class="title-text-holder m-auto pt-4 pb-8 pl-2 pr-2 sm:m-auto w-4/5 block text-center">
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
                     Do you want to become a developer?
                 </h1>
