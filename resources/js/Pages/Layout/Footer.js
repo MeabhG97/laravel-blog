f427111ebb50c4f3645255d9e0eaba9e1892a262
Meabh <91346624+MeabhG97@users.noreply.github.com>
2023-05-12 13:20:20 +0100

    Changed class to classname

--- resources/js/Pages/Layout/Footer.js
+++ resources/js/Pages/Layout/Footer.js
@@ -1,31 +1,31 @@
 import React from "react";
 
-export default function Layout(){
+export default function Footer(){
     return(
-        <footer class="bg-gray-800 py-20 mt-20">
-            <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+        <footer className="bg-gray-800 py-20 mt-20">
+            <div className="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
                 <div>
-                    <h3 class="text-l sm:font-bold text-gray-100">
+                    <h3 className="text-l sm:font-bold text-gray-100">
                         Pages
                     </h3>
 
-                    <ul class="py-4 sm:text-s pt-4 text-gray-400">
-                        <li class="pb-1">
+                    <ul className="py-4 sm:text-s pt-4 text-gray-400">
+                        <li className="pb-1">
                             <a href="/">
                                 Home
                             </a>
                         </li>
-                        <li class="pb-1">
+                        <li className="pb-1">
                             <a href="/blog">
                                 Blog
                             </a>
                         </li>
-                        <li class="pb-1">
+                        <li className="pb-1">
                             <a href="/login">
                                 Login
                             </a>
                         </li>
-                        <li class="pb-1">
+                        <li className="pb-1">
                             <a href="/register">
                                 Register
                             </a>
@@ -34,27 +34,27 @@ export default function Layout(){
                 </div>
 
                 <div>
-                    <h3 class="text-l sm:font-bold text-gray-100">
+                    <h3 className="text-l sm:font-bold text-gray-100">
                         Find Us
                     </h3>
 
-                    <ul class="py-4 sm:text-s pt-4 text-gray-400">
-                        <li class="pb-1">
+                    <ul className="py-4 sm:text-s pt-4 text-gray-400">
+                        <li className="pb-1">
                             <a href="/">
                                 What we do
                             </a>
                         </li>
-                        <li class="pb-1">
+                        <li className="pb-1">
                             <a href="/">
                                 Address
                             </a>
                         </li>
-                        <li class="pb-1">
+                        <li className="pb-1">
                             <a href="/">
                                 Phone
                             </a>
                         </li>
-                        <li class="pb-1">
+                        <li className="pb-1">
                             <a href="/">
                                 Contact
                             </a>
@@ -63,27 +63,27 @@ export default function Layout(){
                 </div>
 
                 <div>
-                    <h3 class="text-l sm:font-bold text-gray-100">
+                    <h3 className="text-l sm:font-bold text-gray-100">
                         Latest posts
                     </h3>
 
-                    <ul class="py-4 sm:text-s pt-4 text-gray-400">
-                        <li class="pb-1">
+                    <ul className="py-4 sm:text-s pt-4 text-gray-400">
+                        <li className="pb-1">
                             <a href="/">
                                 Why we love tech
                             </a>
                         </li>
-                        <li class="pb-1">
+                        <li className="pb-1">
                             <a href="/">
                                 Why we love design
                             </a>
                         </li>
-                        <li class="pb-1">
+                        <li className="pb-1">
                             <a href="/">
                                 Why to use Laravel
                             </a>
                         </li>
-                        <li class="pb-1">
+                        <li className="pb-1">
                             <a href="/">
                                 Why PHP is the best
                             </a>
@@ -91,7 +91,7 @@ export default function Layout(){
                     </ul>
                 </div>
             </div>
-            <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+            <p className="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
                 Copyright 2017-2021 Code With Dary. All Rights Reserved
             </p>
         </footer>
