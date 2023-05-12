33bab0c01c1da6683cc240295dd3c369d74e83d9
Cian <cianashby1337@gmail.com>
2023-05-12 15:11:33 +0100

    4 most recent posts now retrieved for bottom grid

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PagesController extends Controller
 {
     public function index()
     {
-        return view('index');
+        return view('index')->with('posts', DB::select('select * from posts order by created_at desc limit 4'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,25 +47,28 @@
     </div>
 
     <div class="sm:grid grid-cols-2 w-4/5 m-auto">
-        <div class="flex bg-yellow-700 text-gray-100 pt-10">
-            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
-                <span class="uppercase text-xs">
-                    Post title
-                </span>
+        @php
+            foreach ($posts as $post) {
+            
+            echo '<div class="flex bg-yellow-700 text-gray-100 pt-10">
+                    <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
+                        <span class="uppercase text-xs">' .
+                            $post->title
+                        . '</span>
 
-                <h3 class="text-xl font-bold py-10">
-                    Post description (no >250)
-                </h3>
+                        <h3 class="text-xl font-bold py-10">';
+                            if(Str::length($post->description) > 125) echo nl2br(htmlspecialchars(Str::substr($post->description, 0, 125))) . '...';
+                            else echo nl2br(htmlspecialchars($post->description));
+                        echo '</h3>
 
-                <a 
-                    href=""
-                    class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
-                    Read more
-                </a>
-            </div>
-        </div>
-        <div>
-            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" alt="">
-        </div>
+                        <a 
+                            href=""
+                            class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                            Read more
+                        </a>
+                    </div>
+                </div>';
+            }
+        @endphp
     </div>
 @endsection
