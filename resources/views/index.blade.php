1ff645c28dadf94b18989bb42fe9ebe04889ff75
Cian <cianashby1337@gmail.com>
2023-05-12 13:59:04 +0100

    Removed unnecessary web developer section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,59 +5,33 @@
         <div class="flex text-gray-100 pt-10">
             <div class="title-text-holder m-auto pt-4 pb-8 pl-2 pr-2 sm:m-auto w-4/5 block text-center">
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+                    READY TO JUMP INTO ENGAGING PROGRAMMING DISCUSSIONS?
                 </h1>
                 <a 
                     href="/blog"
-                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
-                    Read More
+                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl">
+                    Click Here!
                 </a>
             </div>
         </div>
     </div>
 
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
-        <div>
-            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
-        </div>
-
-        <div class="m-auto sm:m-auto text-left w-4/5 block">
-            <h2 class="text-3xl font-extrabold text-gray-600">
-                Struggling to be a better web developer?
-            </h2>
-            
-            <p class="py-8 text-gray-500 text-s">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
-            </p>
-
-            <p class="font-extrabold text-gray-600 text-s pb-9">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos molestias porro vero. Vel alias.
-            </p>
-
-            <a 
-                href="/blog"
-                class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
-                Find Out More
-            </a>
-        </div>
-    </div>
-
     <div class="text-center p-15 bg-black text-white">
         <h2 class="text-2xl pb-5 text-l"> 
-            I'm an expert in...
+            I want to learn...
         </h2>
 
         <span class="font-extrabold block text-4xl py-1">
-            Ux Design
+            PHP
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Project Management
+            Java
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Digital Strategy
+            C++
         </span>
         <span class="font-extrabold block text-4xl py-1">
-            Backend Development
+            Python
         </span>
     </div>
 
